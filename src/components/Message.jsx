eb104d9b612db695af515f3ac068494faff85969
christina-baldwin <christina.baldwin13@yahoo.com>
2025-04-29 11:20:11 +0200

    adding a dislike button and functionality

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,18 +3,26 @@ import React, { useState } from "react";
 const Message = (props) => {
   const [count, setCount] = useState(0);
 
-  const handleCount = () => {
+  const handleLike = () => {
     setCount((count) => count + 1);
-    console.log(count);
+  };
+
+  const handleDislike = () => {
+    if (count > 0) {
+      setCount((count) => count - 1);
+    }
   };
 
   return (
     <div>
       <p>{props.message}</p>
-      <button onClick={handleCount}>
+      <button onClick={handleLike}>
         <ion-icon name="heart"></ion-icon>
       </button>
       <div>x {count}</div>
+      <button onClick={handleDislike}>
+        <ion-icon name="heart-dislike"></ion-icon>
+      </button>
       <div>{props.time} ago</div>
     </div>
   );
