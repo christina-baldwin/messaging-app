52de530a62097c6f51a10ee8e136aea2c596be3e
christina-baldwin <christina.baldwin13@yahoo.com>
2025-05-08 14:30:27 +0200

    not needing animate porescence

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -4,24 +4,21 @@ import { motion, AnimatePresence } from "framer-motion";
 const Messages = (props) => {
   return (
     <div className="flex flex-col gap-12">
-      <AnimatePresence>
-        {props.messages.map((message) => (
-          <motion.div
-            key={message._id}
-            initial={{ opacity: 0, scale: 0.8 }}
-            animate={{ opacity: 1, scale: 1 }}
-            exit={{ opacity: 0, scale: 0.8 }}
-            transition={{ duration: 0.35, ease: "easeOut" }}
-          >
-            <Message
-              id={message._id}
-              message={message.message}
-              time={message.createdAt}
-              likes={message.hearts}
-            />
-          </motion.div>
-        ))}
-      </AnimatePresence>
+      {props.messages.map((message) => (
+        <motion.div
+          key={message._id}
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.35, ease: "easeOut" }}
+        >
+          <Message
+            id={message._id}
+            message={message.message}
+            time={message.createdAt}
+            likes={message.hearts}
+          />
+        </motion.div>
+      ))}
     </div>
   );
 };
