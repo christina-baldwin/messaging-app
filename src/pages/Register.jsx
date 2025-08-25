9c5e3a1c4355a290dc01f1868826462988cdf836
christina-baldwin <christina.baldwin13@yahoo.com>
2025-08-25 13:11:53 +0200

    added a navigate back to login on succesful registration so UX is clearer

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Register = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const [error, setError] = useState("");
+
+  const navigate = useNavigate();
 
   const handleRegister = async (e) => {
     e.preventDefault();
@@ -71,6 +73,8 @@ const Register = () => {
       setEmail("");
       setPassword("");
       setError("");
+
+      navigate("/login");
     } catch (error) {
       setError(error.message);
     }
